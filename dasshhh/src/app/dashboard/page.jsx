8fcf0b37398a902fda27a16dd9ca35bbca53086e
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-01-09 19:26:56 +0530

    added project structure, navbar and dashboard

---
+++ dasshhh/src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Layout from "../components/layout";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Layout>
+
+      </Layout>
+    </>
+  );
+}
