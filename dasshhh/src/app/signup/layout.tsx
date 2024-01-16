f91b196f698ab831d751ddd61bd45a5092a0fbf0
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-01-16 19:16:47 +0530

    Fixed the issues in backend and complete the Dashboard.

--- dasshhh/src/app/signup/layout.tsx
+++ dasshhh/src/app/signup/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Login | Next Dashboard | rajivgupta.rf.gd",
+  title: "Signup | Next Dashboard | rajivgupta.rf.gd",
   description: "Advance Next Dashboard",
 };
 
